459f75d97040e4259394b682c50d20f50dbf62f6
Simon L. <72554879+visviva@users.noreply.github.com>
2026-09-25 21:48:51 +0200

    Fix formatting

--- Syntax/ParserException.cs
+++ Syntax/ParserException.cs
@@ -10,7 +10,10 @@ internal sealed class ParserException : Exception
         : base($"Unexpected token: {token} at position: {token.Position}, expected {expected}") { }
 
     public ParserException(SyntaxToken token, SyntaxKind expected1, SyntaxKind expected2)
-        : base(
-            $"Unexpected token: {token} at position: {token.Position}, expected {expected1} or {expected2}"
-        ) { }
+        : base(CreateMessage(token, expected1, expected2)) { }
+
+    private static string CreateMessage(SyntaxToken token, SyntaxKind expected1, SyntaxKind expected2)
+    {
+        return $"Unexpected token: {token} at position: {token.Position}, expected {expected1} or {expected2}";
+    }
 }
